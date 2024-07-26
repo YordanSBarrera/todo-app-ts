9581d45204514b68a334d32147834c24e2e680b0
Yordan Saldaña <yordansaldanabarrera@gmail.com>
2024-07-26 14:47:27 -0300

    include style from todomvc-app-css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+const mockTodos = [
+  { id: "1", title: "Revisar repo", completed: "false" },
+  { id: "2", title: "Create CV", completed: "true" },
+  { id: "3", title: "Create Portafolio", completed: "false" },
+  { id: "4", title: "Update CV", completed: "false" },
+];
 const App = () => {
-  return <>Hola Mundo</>;
+  return <h1>Hola Mundo</h1>;
 };
 
 export default App;
