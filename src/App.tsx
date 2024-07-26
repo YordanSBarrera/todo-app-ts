8adbc996d61c5fa23e21d78283427d5552dd9d8f
Yordan Saldaña <yordansaldanabarrera@gmail.com>
2024-07-26 16:12:30 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
-const mockTodos = [
-  { id: "1", title: "Revisar repo", completed: "false" },
-  { id: "2", title: "Create CV", completed: "true" },
-  { id: "3", title: "Create Portafolio", completed: "false" },
-  { id: "4", title: "Update CV", completed: "false" },
+import { useState } from "react";
+import Todos from "./components/Todos";
+
+export type TodoItem = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+const mockTodos: TodoItem[] = [
+  { id: "1", title: "Revisar repo", completed: false },
+  { id: "2", title: "Create CV", completed: true },
+  { id: "3", title: "Create Portafolio", completed: false },
+  { id: "4", title: "Update CV", completed: false },
 ];
 const App = () => {
-  return <h1>Hola Mundo</h1>;
+  const [todos, setTodos] = useState(mockTodos);
+
+  return (
+    <div className="todoapp">
+      <h1>Mis tareas</h1>
+      <Todos todos={todos} />
+    </div>
+  );
 };
 
 export default App;
